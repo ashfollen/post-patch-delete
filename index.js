93ec5b202f0377be29886308825361a5e22668dd
Aysan Isayo <aisayo@icloud.com>
2021-07-27 15:22:30 -0700

    patch

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const pokeContainer = document.getElementById("poke-container");
-const pokeForm = document.getElementById('poke-form')
+const pokeForm = document.getElementById("poke-form");
 
 function renderPoke(pokemon) {
   const pokeCard = document.createElement("div");
   pokeCard.id = `poke-${pokemon.id}`;
   pokeCard.className = "poke-card";
-  pokeCard.dataset.id = pokemon.id
+  pokeCard.dataset.id = pokemon.id;
 
   const pokeImg = document.createElement("img");
   pokeImg.src = pokemon.img;
@@ -26,71 +26,90 @@ function renderPoke(pokemon) {
   likeBttn.textContent = "<3";
   likeBttn.addEventListener("click", increaseLike);
 
-  const deleteBttn = document.createElement("button")
-  deleteBttn.className = 'delete-bttn'
-  deleteBttn.textContent = 'Delete'
+  const br = document.createElement("br");
+
+  const deleteBttn = document.createElement("button");
+  deleteBttn.className = "delete-bttn";
+  deleteBttn.textContent = "Delete";
   deleteBttn.addEventListener("click", deletePokemon);
 
-
-  pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
+  pokeCard.append(
+    pokeImg,
+    pokeName,
+    pokeLikes,
+    likesNum,
+    likeBttn,
+    br,
+    deleteBttn
+  );
   pokeContainer.appendChild(pokeCard);
 }
 
-function deletePokemon(e){
-  e.target.parentElement.remove()
+function deletePokemon(e) {
+  e.target.parentElement.remove();
   fetch(`http://localhost:3000/pokemons/${e.target.parentElement.dataset.id}`, {
-    method: 'DELETE'
-  })
-
+    method: "DELETE",
+  });
 }
 
 function increaseLike(e) {
   const likesElement = e.target.previousElementSibling;
-  likesElement.textContent = parseInt(likesElement.textContent) + 1;
-}
-
-function createPoke(e){
-  e.preventDefault();
-  const pForm = e.target
-  const pokeName = pForm.querySelector('#name-input').value
-  const pokeImg = pForm.querySelector('#img-input').value
-
-  if (pokeName !== '' && pokeImg !== ''){
-    const poke = {
-      name: pokeName,
-      img: pokeImg,
-      likes: 0
-    }
-
-    const configObj = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(poke)
-    }
- 
-    fetch('http://localhost:3000/pokemons', configObj)
-    .then(resp => resp.json())
-    .then(renderPoke)
-    pokeForm.reset()
-  } else {
-    alert('Fill in the form!!!')
-  }
-  
-}
-
-function getPokemons(){
-  fetch('http://localhost:3000/pokemons')
+  const id = e.target.parentElement.dataset.id
+  const updatedLikes = parseInt(likesElement.textContent) + 1
+  fetch(`http://localhost:3000/pokemons/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({likes: updatedLikes})
+  })
   .then(resp => resp.json())
-  .then(pokemons => {
-    pokemons.forEach(renderPoke)
+  .then(pokemon => {
+    likesElement.textContent = pokemon.likes
   })
 }
 
+function createPoke(e) {
+  e.preventDefault();
+  const pForm = e.target;
+  const pokeName = pForm.querySelector("#name-input").value;
+  const pokeImg = pForm.querySelector("#img-input").value;
+
+  if (pokeName !== "" && pokeImg !== "") {
+    const poke = {
+      name: pokeName,
+      img: pokeImg,
+      likes: 0,
+    };
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(poke),
+    };
+
+    fetch("http://localhost:3000/pokemons", configObj)
+      .then((resp) => resp.json())
+      .then(renderPoke);
+    pokeForm.reset();
+  } else {
+    alert("Fill in the form!!!");
+  }
+}
+
+function getPokemons() {
+  fetch("http://localhost:3000/pokemons")
+    .then((resp) => resp.json())
+    .then((pokemons) => {
+      pokemons.forEach(renderPoke);
+    });
+}
+
 function init() {
-  getPokemons()
-  pokeForm.addEventListener('submit', createPoke)
+  getPokemons();
+  pokeForm.addEventListener("submit", createPoke);
 }
 
 init();
