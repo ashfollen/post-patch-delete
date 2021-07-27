12bfae27ce4229a1da849801c0ba159050fb0b8e
Aysan Isayo <aisayo@icloud.com>
2021-07-27 13:38:25 -0700

    updated code to reflect pokedex design

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-For this exercise, we will be using the following API: https://www.themealdb.com/api.php
-
-At the moment, a few issues exist with the current code in `index.js`.
-
-1. The fetch request is hardcoded and will always return `chicken` meals: `https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`
-    - `s=chicken` is the parameter provided to the endpoint that accepts a query term and returns a response based on the value after the `=`.
-    - The goal is to update the current code and create a dynamic parameter based on the users entry. 
-    - Action items: 
-        - The fetch request should only happen after the form is submitted
-        - When user submits the search form, select and return the value of the input.
-        - Dynamically apply the input value to the endpoint currently passed to fetch.
-
-2. Open index.html in the browser and inspect the console to see if the anticipated data is being received from the request. Review the code to identify what needs to be updated to correct the current output. 
+Run `json-server --watch db.json`
--- index.js
+++ index.js
@@ -1,3 +1,71 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken')
-.then(resp => resp)
-.then(data => console.log(data))
+const pokeContainer = document.getElementById("poke-container");
+const pokeForm = document.getElementById('poke-form')
+
+function renderPoke(pokemon) {
+  const pokeCard = document.createElement("div");
+  pokeCard.id = `poke-${pokemon.id}`;
+  pokeCard.className = "poke-card";
+
+  const pokeImg = document.createElement("img");
+  pokeImg.src = pokemon.img;
+  pokeImg.alt = `${pokemon.name} image`;
+
+  const pokeName = document.createElement("h3");
+  pokeName.textContent = pokemon.name;
+
+  const pokeLikes = document.createElement("h3");
+  pokeLikes.textContent = "Likes: ";
+
+  const likesNum = document.createElement("h5");
+  likesNum.id = "like-num";
+  likesNum.textContent = pokemon.likes;
+
+  const bttn = document.createElement("button");
+  bttn.id = "like-bttn";
+  bttn.textContent = "<3";
+  bttn.addEventListener("click", increaseLike);
+
+  pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, bttn);
+  pokeContainer.appendChild(pokeCard);
+}
+
+function increaseLike(e) {
+  const likesElement = e.target.previousElementSibling;
+  likesElement.textContent = parseInt(likesElement.textContent) + 1;
+}
+
+function createPoke(e){
+  e.preventDefault();
+  const pForm = e.target
+  const pokeName = pForm.querySelector('#name-input').value
+  const pokeImg = pForm.querySelector('#img-input').value
+
+  if (pokeName !== '' && pokeImg !== ''){
+    const poke = {
+      id: 7, // hard coded can be changed to  be more dynamic
+      name: pokeName,
+      img: pokeImg,
+      likes: 0
+    }
+    renderPoke(poke)
+    pokeForm.reset()
+  } else {
+    alert('Fill in the form!!!')
+  }
+  
+}
+
+function getPokemons(){
+  fetch('http://localhost:3000/pokemons')
+  .then(resp => resp.json())
+  .then(pokemons => {
+    pokemons.forEach(renderPoke)
+  })
+}
+
+function init() {
+  getPokemons()
+  pokeForm.addEventListener('submit', createPoke)
+}
+
+init();
