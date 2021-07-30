9fd0cc763c4dcf0cafbb860672eed1675cce6ca5
Ashley Follen <ashleyfollen@outlook.com>
2021-07-30 14:04:31 -0600

    lecture complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const url = "http://localhost:3000/pokemons"
 const pokeContainer = document.getElementById("poke-container");
 const pokeForm = document.getElementById("poke-form");
 
@@ -23,19 +24,42 @@ function renderPoke(pokemon) {
   const likeBttn = document.createElement("button");
   likeBttn.className = "like-bttn";
   likeBttn.textContent = "♥";
-  likeBttn.addEventListener("click", increaseLike);
+  likeBttn.addEventListener("click", () => increaseLike(pokemon)); 
 
   const deleteBttn = document.createElement("button");
   deleteBttn.className = "delete-bttn";
   deleteBttn.textContent = "Delete";
+  deleteButton.addEventListener('click', () => deletePokemon(pokemon))
 
   pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
 }
 
-function increaseLike(e) {
-  const likesElement = e.target.previousElementSibling;
-  likesElement.textContent = parseInt(likesElement.textContent) + 1;
+function deletePokemon(pokemon) {
+  //make a fetch request DELETE
+  //forced optimistic since we don't need the .then's
+  document.getElementById(`poke-${pokemon.id}`).remove()
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: "DELETE"
+  })
+  
+}
+
+function increaseLike(pokemon) {
+//should we be pessimistic or optimistic - let's be optimistic
+//PATCH http verb
+//what is my endpoint going to be - the id of our object we're making an update to
+
+  const likesElement = event.target.previousElementSibling;
+  const likes = pokemon.likes += 1;
+  likesElement.textContent = likes;
+
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ likes: likes }) //what the object is that we'll be updating, and the value of the object
+  })
+   // optimistic - don't need to use the info we get back, so no need for .then 
 }
 
 function createPoke(e) {
@@ -46,22 +70,43 @@ function createPoke(e) {
 
   if (pokeName !== "" && pokeImg !== "") {
     const poke = {
-      id: 7, // hard coded can be changed to  be more dynamic
+      //id: 7, remove id property because the api will create its own id
       name: pokeName,
       img: pokeImg,
       likes: 0,
     };
-    renderPoke(poke);
+
+  const configObj = {
+    method: 'POST', //if you don't want to make a default GET request, you need to include a method to overwrite it
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(poke), 
+    }
+
+    fetch(url, configObj)
+    .then(resp => resp.json())
+    .then(renderPoke); // pessimistic - wait until the server approves the POST before adding it to the DOM
+    
     pokeForm.reset();
   } else {
     alert("Fill in the form!!!");
   }
 }
 
-// function getPokemons() {
-// }
+function getPokemons() {
+  // render all pokemons on page
+
+  //first step
+  fetch(url) 
+  .then(resp => resp.json()) // returns a promise
+  .then(pokemons => 
+    pokemons.forEach(renderPoke)) // returns another promise
+} // name the parameter obvious as to what the dataset we're working with is
+// run forEach so that renderPoke function later isn't looking for an array
 
 function init() {
+  getPokemons() //add this here so that getPokemons is being called
   pokeForm.addEventListener("submit", createPoke);
 }
 
